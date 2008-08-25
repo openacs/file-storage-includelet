ac3174e63eb69dad977933e3cdff7853bfa0bd9d
Don Baccus <dhogaza@pacifier.com>
2008-08-25 20:25:17 +0000

    The URL for the file storage package instance was hardwired to be "file-storage", which I guess worked OK in .LRN where the portlet apparently knows where the applet puts it but ... still ... argh!

--- lib/file-storage-admin-includelet.tcl
+++ lib/file-storage-admin-includelet.tcl
@@ -12,8 +12,6 @@ set return_url [ns_conn url]
 
 set user_id [ad_conn user_id]
 
-ad_return_template
-
-set package_id [site_node_apm_integration::get_child_package_id -package_key "file-storage"]
+set url [site_node::get_url_from_object_id -object_id $package_id]
 set fs_url "/shared/parameters?[export_url_vars package_id return_url]"
 set show_fs_url_p [parameter::get_from_package_key -parameter ShowParametersLinkP -package_key file-storage-includelet -default 1]
